ac002c8bf2d1d0475a9705cb6088fd44759c003f
Septian Adi Pratama <septianadip7998@gmail.com>
2024-10-28 16:27:20 +0700

    axios etc

--- client/src/Pages/Posts/Posts.jsx
+++ client/src/Pages/Posts/Posts.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Posts.css";
+import axios from "axios";
+
 const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+    const {data} = await axios
+    }
+  });
+
   return (
     <div className="posts">
       <div className="container">
